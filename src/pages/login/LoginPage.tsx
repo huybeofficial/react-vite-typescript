990a2350b231dbcca107e34d04a92931d92f24a1
huybe <huybeofficial@gmail.com>
2024-11-22 11:36:53 +0700

    init base

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useRoutes } from 'react-router'
+
+const route = useRoutes([
+
+])
+
+const LoginPage = () => {
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default LoginPage
